57cd624fcdc90cb53392acb8fac086857586d5dc
Felipe Alves de Jesus <felipe.jesus@ext.vallourec.com>
2023-06-03 14:49:37 -0300

    deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,41 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import postcssConfig from "./postcss.config.js";
+import postcssConfig from './postcss.config.js';
 
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? "https://tudoehpureza.github.io"
-    : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? "/front-domains" : undefined,
-  //output: 'server',
-  //adapter: cloudflare(),
+	site: process.env.CI
+		? 'https://tudoehpureza.github.io'
+		: `http://localhost:${DEV_PORT}`,
+	base: process.env.CI ? '/front-domains' : undefined,
+	//output: 'server',
+	//adapter: cloudflare(),
 
-  /* Like Vercel, Netlify,… Mimicking for dev. server */
-  // trailingSlash: 'always',
+	/* Like Vercel, Netlify,… Mimicking for dev. server */
+	// trailingSlash: 'always',
 
-  server: {
-    /* Dev. server only */
-    port: DEV_PORT,
-  },
-  integrations: [
-    //
-    sitemap(),
-    tailwind(),
-    react(),
-  ],
-  build: {
-    postcss: postcssConfig,
-  },
-  vite: {
-    build: {
-      minify: false,
-    },
-  },
+	server: {
+		/* Dev. server only */
+		port: DEV_PORT,
+	},
+	integrations: [
+		//
+		sitemap(),
+		tailwind(),
+		react(),
+	],
+	build: {
+		postcss: postcssConfig,
+	},
+	vite: {
+		build: {
+			minify: false,
+		},
+	},
 });
